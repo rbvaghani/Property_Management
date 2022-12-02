d86495356a2f5ec3a7c6e751f542109c00c5cc2d
mansiu08 <114713185+mansiu08@users.noreply.github.com>
2022-12-02 18:47:58 -0500

    added code to user interface

--- src/Business/UserAccount/UserAccount.java
+++ src/Business/UserAccount/UserAccount.java
@@ -3,11 +3,83 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.UserAccount;
+import Business.Ecosystem;
+import Business.Employee.Employee;
+import Business.Enterprise.Enterprise;
+import Business.Organisation.Organisation;
+import Business.Role.Role;
+import Business.WorkQueue.WorkQueue;
+import javax.swing.JPanel;
 
 /**
  *
  * @author mansi
  */
 public class UserAccount {
+    public class UserAccount {
+    
+    private String userName;
+    private String password;
+    private Role role;
+    private Employee employee;
+    private WorkQueue workQueue;
+    
+    public UserAccount()
+    {
+        workQueue = new WorkQueue();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public void setWorkQueue(WorkQueue workQueue) {
+        this.workQueue = workQueue;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return userName;
+    }
+
+
+
+
+
+    
+}
     
 }
