0c76b9a92043fbbdf8df3bb13243f804734a2d6c
Ram Vaghani <vaghani.r@northeastern.edu>
2022-12-02 21:43:30 -0500

    created customer employee enterprise

---
+++ src/Business/DB40Util/DB4OUtil.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.DB40Util;
+
+/**
+ *
+ * @author ramvaghani
+ */
+public class DB4OUtil {
+    
+}
