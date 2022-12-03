ae4ea482f2f0233e77f649d9affc300545dd5fbd
mansiu08 <114713185+mansiu08@users.noreply.github.com>
2022-12-03 18:17:23 -0500

    added code

--- src/Business/Utility/Event.java
+++ src/Business/Utility/Event.java
@@ -4,10 +4,72 @@
  */
 package Business.Utility;
 
+import Business.Customer.Customer;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
 /**
  *
  * @author mansi
  */
 public class Event {
     
-}
+    private String eventName;
+    private String eventDescription;
+    private int eventCapacity;
+    private int addedSeat;
+    private int vacantSeat;
+    private Date eventDate;
+    private Customer eventAdmin;
+    private String location;
+    private ArrayList<String> eventChatBox;
+    private String status;
+    private HashMap<Customer,Integer> attendeesList;
+    
+    
+    public Event(String eventName, String  eventDescription,int eventCapacity, Date eventDate,String location){
+        this.eventName=eventName;
+        this.eventDescription=eventDescription;
+        this.eventCapacity=eventCapacity;
+        addedSeat=0;
+        vacantSeat=eventCapacity;
+        this.eventDate=eventDate;
+        eventChatBox= new ArrayList<>();
+        eventChatBox.add("Start of chat");
+        eventChatBox.add("Have fun :)");
+        status="PENDING";
+        this.location=location;
+        attendeesList=new HashMap<>();
+        
+    }
+
+    public HashMap<Customer, Integer> getAttendeesList() {
+        return attendeesList;
+    }
+
+    public void setAttendeesList(HashMap<Customer, Integer> attendeesList) {
+        this.attendeesList = attendeesList;
+    }
+    
+    
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public ArrayList<String> getEventChatBox() {
+        return eventChatBox;
+    }
+
+    public void setEventChatBox(ArrayList<String> eventChatBox) {
+        this.eventChatBox = eventChatBox;
+    }
+
+    public String getStatus() {
+        return status;
+    }
