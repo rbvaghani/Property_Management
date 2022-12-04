bd775fdee9679efc5f6f29bae26bdc9532b17fcc
Ram Vaghani <vaghani.r@northeastern.edu>
2022-12-04 02:28:05 -0500

    configured security society

--- src/Business/Enterprise/SecurityEnterprise.java
+++ src/Business/Enterprise/SecurityEnterprise.java
@@ -1,13 +1,32 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Enterprise;
+
+import Business.Role.GovernmentEnterpriseRole;
+import Business.Role.Role;
+import Business.Role.SecurityEnterpriseRole;
+import java.util.ArrayList;
 
 /**
  *
  * @author ramvaghani
  */
-public class SecurityEnterprise {
+public class SecurityEnterprise extends Enterprise {
+    
+    public SecurityEnterprise(String name,Enterprise.EnterpriseType type) {
+        super(name,type.Security);
+    }
+    
+    
+@Override
+public ArrayList<Role> getSupportedRole() {
+    ArrayList<Role> roles = new ArrayList<>();
+    roles.add(new SecurityEnterpriseRole());
+    return roles; //To change body of generated methods, choose Tools | Templates.
+}
+    
     
 }
--- src/Business/Enterprise/SocietyEnterprise.java
+++ src/Business/Enterprise/SocietyEnterprise.java
@@ -1,13 +1,30 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Enterprise;
+
+import Business.Role.Role;
+import Business.Role.SecurityEnterpriseRole;
+import Business.Role.SocietyEnterpriseRole;
+import java.util.ArrayList;
 
 /**
  *
  * @author ramvaghani
  */
-public class SocietyEnterprise {
+public class SocietyEnterprise extends Enterprise{
+    
+    public SocietyEnterprise(String name,Enterprise.EnterpriseType type) {
+        super(name,type.Society);
+    }
+    
+@Override
+public ArrayList<Role> getSupportedRole() {
+    ArrayList<Role> roles = new ArrayList<>();
+    roles.add(new SocietyEnterpriseRole());
+    return roles; //To change body of generated methods, choose Tools | Templates.
+}
     
 }
