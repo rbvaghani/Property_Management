0b9b8403c59769a607eda2b83bdbc232fa6a1fbb
Ram Vaghani <vaghani.r@northeastern.edu>
2022-12-04 01:59:21 -0500

    created network organization interface

--- src/Business/Employee/EmployeeDirectory.java
+++ src/Business/Employee/EmployeeDirectory.java
@@ -1,13 +1,47 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Employee;
+
+import Business.Role.Role;
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
 
 /**
  *
  * @author ramvaghani
  */
 public class EmployeeDirectory {
+    
+    private ArrayList<Employee> employeeDirectory;
+    
+    public EmployeeDirectory(){
+        employeeDirectory= new ArrayList<>();
+        
+    }
+
+    public ArrayList<Employee> getEmployeeDirectory() {
+        return employeeDirectory;
+    }
+    
+    
+    public Employee createEmployee(String name, Role role)
+    {
+        Employee employee = new Employee();
+        employee.setName(name);
+        employee.setRole(role);
+        
+        //employee.setEmpUserAcc(empUserAcc);
+        /////
+        //ROLE and WORK QUEUE????
+        /////
+        employeeDirectory.add(employee);
+        return employee;
+    
+    }
+    
+    
     
 }
--- src/Business/Employee/SecurityEmployee.java
+++ src/Business/Employee/SecurityEmployee.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package Business.Employee;
 
@@ -8,6 +9,34 @@ package Business.Employee;
  *
  * @author ramvaghani
  */
-public class SecurityEmployee {
+public class SecurityEmployee extends Employee{
+    
+    private String streetName;
+    private String timeSlot;
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    public void setTimeSlot(String timeSlot) {
+        this.timeSlot = timeSlot;
+    }
+
+    @Override
+    public String toString() {
+        return super.getName()+": "+ streetName ;
+    }
+    
+    
+    
+    
     
 }
