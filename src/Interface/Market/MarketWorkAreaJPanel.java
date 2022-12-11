c6eda928173217f957d3956e6539f4f0e2f8c356
mansiu08 <114713185+mansiu08@users.noreply.github.com>
2022-12-11 16:23:54 -0500

    manage work areapanel edited

--- src/Interface/Market/MarketWorkAreaJPanel.java
+++ src/Interface/Market/MarketWorkAreaJPanel.java
@@ -26,19 +26,151 @@ public class MarketWorkAreaJPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        manageProducts = new javax.swing.JButton();
+        ManageOrdersBtn = new javax.swing.JButton();
+        ManageSupAccountsJPanel = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        nameTxtField = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+
+        jPanel1.setBackground(new java.awt.Color(153, 153, 0));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("Market Work Area");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(301, 10, -1, -1));
+
+        manageProducts.setText("Manage Products");
+        manageProducts.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                manageProductsActionPerformed(evt);
+            }
+        });
+        jPanel1.add(manageProducts, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 42, 290, -1));
+
+        ManageOrdersBtn.setText("Manage Orders");
+        ManageOrdersBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ManageOrdersBtnActionPerformed(evt);
+            }
+        });
+        jPanel1.add(ManageOrdersBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 83, 290, -1));
+
+        ManageSupAccountsJPanel.setText("Manage Retailer Automatic Requests");
+        ManageSupAccountsJPanel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ManageSupAccountsJPanelActionPerformed(evt);
+            }
+        });
+        jPanel1.add(ManageSupAccountsJPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 124, 290, -1));
+
+        jButton1.setText("Market Wallet ");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 168, 290, -1));
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Welcome !");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, -1));
+        jPanel1.add(nameTxtField, new org.netbeans.lib.awtextra.AbsoluteConstraints(121, 10, 120, -1));
+
+        jButton2.setText("Analytics");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 209, 290, -1));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 0, -1, -1));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 778, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 778, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 475, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 475, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void manageProductsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manageProductsActionPerformed
+        // TODO add your handling code here:
+        Interface.Market.ManageProductsJPanel mpjp = new Interface.Market.ManageProductsJPanel(userProcessContainer,account,organisation1,enterprise,business);
+        userProcessContainer.add("ManageProductsJPanel",mpjp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_manageProductsActionPerformed
+
+    private void ManageOrdersBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ManageOrdersBtnActionPerformed
+        // TODO add your handling code here:
+        Interface.Market.ManageOrdersJPanel immojp = new Interface.Market.ManageOrdersJPanel(userProcessContainer,account,organisation1,enterprise,business);
+        userProcessContainer.add("ManageOrdersJPanel",immojp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_ManageOrdersBtnActionPerformed
+
+    private void ManageSupAccountsJPanelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ManageSupAccountsJPanelActionPerformed
+        // TODO add your handling code here:
+
+        Interface.Market.ManageGroceryEmployeesJPanel immgejp = new Interface.Market.ManageGroceryEmployeesJPanel(userProcessContainer,account,organization,enterprise,business,retailerOrganisation);
+        userProcessContainer.add("ManageGroceryEmployeesJPanel",immgejp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_ManageSupAccountsJPanelActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+
+        Wallet wallet= grocery.getWalletObj();
+        String name= grocery.getOrgName().toString();
+        MarketWalletDetails mwdjp = new MarketWalletDetails(name, wallet, userProcessContainer);
+        userProcessContainer.add("mwdjp", mwdjp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+
+        Interface.Market.AnalyticsJPanel immajppp = new Interface.Market.AnalyticsJPanel(userProcessContainer,account,grocery,enterprise,business,retailerOrganisation);
+        userProcessContainer.add("AnalyticsJPanel",immajppp);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+
+    }//GEN-LAST:event_jButton2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ManageOrdersBtn;
+    private javax.swing.JButton ManageSupAccountsJPanel;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton manageProducts;
+    private javax.swing.JTextField nameTxtField;
     // End of variables declaration//GEN-END:variables
 }
